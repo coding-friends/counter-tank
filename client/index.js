82721c93381edfd37e1db9696e2c2d29346507e1
James Liang <james.liang.cje@icloud.com>
2020-11-03 19:35:22 -0500

    added canvas and p5

---
+++ client/index.js
@@ -0,0 +1,12 @@
+const socket = io()
+console.log("connected")
+
+function setup(){
+  createCanvas(window.innerWidth,window.innerHeight)
+}
+
+function draw(){
+  background(230)
+  
+
+}
--- server/app.js
+++ server/app.js
@@ -1,26 +1,22 @@
-const express = require("express")
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const path = require("path")
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
 
-const ROOT_DIR = path.dirname(__dirname)
-const CLIENT_DIR = path.join(ROOT_DIR, "client")
-const PORT = process.env.PORT || 8080
-
-app.use(express.static(CLIENT_DIR))
-
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-    console.log(`Server url : http://localhost:${PORT}`)
-})
+const ROOT_DIR = path.dirname(__dirname);
+const CLIENT_DIR = path.join(ROOT_DIR, "client");
+const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
-    console.log("a user connected")
-    socket.on('disconnect',()=>{
-        console.log("user disconnected")
-    })
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
-    socket.on('')
-})
+app.use(express.static(CLIENT_DIR));
+http.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log(`Server url : http://localhost:${PORT}`);
+});
